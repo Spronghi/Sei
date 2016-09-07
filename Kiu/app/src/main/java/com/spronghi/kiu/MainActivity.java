ea00cac7968427ac2750f19257eb41f49f57fd4c
spronghi <sim.colaci@gmail.com>
2016-09-07 17:38:38 +0200

    first push

---
+++ Kiu/app/src/main/java/com/spronghi/kiu/MainActivity.java
@@ -0,0 +1,17 @@
+package com.spronghi.kiu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile_screen);
+        /*
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        */
+    }
+}
---
+++ Kiu/app/src/main/java/com/spronghi/kiu/ViewUserProfileActivity.java
@@ -0,0 +1,11 @@
+package com.spronghi.kiu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by spronghi on 07/09/16.
+ */
+public class ViewUserProfileActivity extends Fragment{
+
+}
